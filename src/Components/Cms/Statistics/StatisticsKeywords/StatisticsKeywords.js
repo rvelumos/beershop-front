d43de5c946ef5aa45e0818676508fdbe20bf27f8
Ronald <ronald.eijsden@gmail.com>
2021-05-31 22:02:38 +0200

    top 10 keywords added on statisticspage

--- src/Components/Cms/Statistics/StatisticsKeywords/StatisticsKeywords.js
+++ src/Components/Cms/Statistics/StatisticsKeywords/StatisticsKeywords.js
@@ -7,11 +7,10 @@ function StatisticsKeywords({token}) {
     const [loading, toggleLoading] = useState(true);
     const [items, setItems] = useState("");
     const [error, setError] = useState("");
-    const [foundKeywords, setFoundKeywords] = useState(false);
+    const [mode, setMode] = useState('init');
 
     async function getTop10Keywords() {
-        toggleLoading(true);
-
+        setMode('data');
         const url = `/api/v1/keywords/top`;
 
         try {
@@ -22,35 +21,38 @@ function StatisticsKeywords({token}) {
                     "Access-Control-Allow-Origin": "*",
                 }
             });
-
+            console.log(result)
             if (result.data.length > 0) {
+                toggleLoading(false)
                 setItems(result.data);
-                setFoundKeywords(true);
             }
-            toggleLoading(false);
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             setError("could not reach external source");
         }
-        toggleLoading(false);
     }
+
+    if(mode === 'init')
+        getTop10Keywords()
     // select keyword, sum (amount) as aantal from search group by keyword order by aantal DESC
 
     function displaySearchResults() {
-        return(
-            items.map((item) => {
-                return(
-                    <p>{item.name} ({item.amount})</p>
-                )
-            })
-        )
+        if(items.length > 0) {
+            const searchItems = Array.from(Object.entries(items));
+            return (
+                searchItems.map((item, i) => {
+                    return (
+                        <p>{item[1].keywordName} ({item[1].keywordAmount}x)</p>
+                    )
+                })
+            )
+        }
     }
 
     return(
         <>
             {error && <Feedback type="error" content={error} />}
-            {loading ? <LoadingIndicator /> : getTop10Keywords()}
-            {foundKeywords && displaySearchResults()}
+            {loading ? <LoadingIndicator /> : displaySearchResults()}
         </>
     )
 }
